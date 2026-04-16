a5a5cf89bbc54a45486e0d1f1de173169f5d5879
Tanmay <tanmayghosh4597@gmail.com>
2026-04-16 23:05:21 +0530

    started backend for the first time

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
-const express = require('express');
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(express.urlencoded({extended:true}));
+app.use(cors({origin:"http://localhost:5173",
+    credentials: true}));
 
 app.get('/', (req, res) => {
     res.send("API is running");
 });
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server started on address http://localhost:${PORT}`);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on address http://localhost:${process.env.PORT}`);
 });
